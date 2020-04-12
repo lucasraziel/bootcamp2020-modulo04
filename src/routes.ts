0eb1322e384578cf72f68aefdbb4b77c9f168455
Lucas <lucas.raziel@gmail.com>
2020-04-12 19:15:46 -0300

    Class 04 - Types interface and array

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,22 @@
 import {Request, Response} from 'express'
 
+import createUser from './services/CreateUser'
+
+
 export function helloworld(request:Request, response:Response){
+
+  const user = createUser({
+    email:'contato@contato.com', 
+    password:'123456', 
+    name:'Lucas',
+    techs:[
+      'ReacJS',
+      'NodeJS',
+      {
+        title:'React Native',
+        experience:30
+      }
+    ]
+  })
   return response.json({message:'Hello World'})
 }
